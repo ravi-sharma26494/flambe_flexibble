7f00f167b5fcff7d9809eb5d787ed6457f03c65e
Ravi Sharma <ravi.sharma26494@gmail.com>
2023-07-24 23:37:12 +0530

    grafbase added

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className="flex-start flex-col paddings md-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>LoadMore</h1>
+    </section>
+  );
+};
+export default Home;
---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,40 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+// Welcome to Grafbase!
+// Define your data models, integrate auth, permission rules, custom resolvers, search, and more with Grafbase.
+// Integrate Auth
+// https://grafbase.com/docs/auth
+//
+// const authProvider = auth.OpenIDConnect({
+//   issuer: process.env.ISSUER_URL ?? ''
+// })
+//
+// Define Data Models
+// https://grafbase.com/docs/database
+
+const User = g.model('User',{
+  name: g.string().length({min:2, max:20}),
+  email: g.string().unique(),
+  avatar: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.string().optional(),
+  linkedInUrl: g.string().optional(),
+  projects: g.relation(()=>Project).list().optional(),
+
+});
+
+const Project = g.model('Project',{
+  titel: g.string().length({min:3}),
+  descriptions: g.string().optional(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.string().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(()=> User)
+  
+
+})
+export default config({
+  schema: g
+
+})
